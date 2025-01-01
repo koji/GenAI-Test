2296389da9d65859c9529479f3bdbeb51968bad5
koji <baxin1919@gmail.com>
2025-01-01 01:34:18 -0500

    clean import statements order

--- ollama/call_ollama_api.py
+++ ollama/call_ollama_api.py
@@ -1,5 +1,6 @@
-import requests
 import json
+
+import requests
 
 url = 'http://localhost:11434/api/generate'
 
--- ollama/pdf-rag.py
+++ ollama/pdf-rag.py
@@ -1,20 +1,16 @@
-from langchain_community.document_loaders import UnstructuredPDFLoader
-from langchain_community.document_loaders import OnlinePDFLoader
-
-import ollama
-from langchain_ollama import OllamaEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+import chromadb
+from langchain.prompts import ChatPromptTemplate, PromptTemplate
+from langchain.retrievers.multi_query import MultiQueryRetriever
 # from langchain_community.vectorstores import Chroma
 from langchain_chroma import Chroma
-
-
-from langchain.prompts import ChatPromptTemplate, PromptTemplate
+from langchain_community.document_loaders import (OnlinePDFLoader,
+                                                  UnstructuredPDFLoader)
 from langchain_core.output_parsers import StrOutputParser
-from langchain_ollama import ChatOllama
 from langchain_core.runnables import RunnablePassthrough
-from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain_ollama import ChatOllama, OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-import chromadb
+import ollama
 
 DOC_PATH = './data/BOI.pdf'
 MODEL = 'llama3.2:latest'
